e69595b0aabcfa8e5de370d2ddaa3a2a5110c3a2
leticiacb1 <leticiacb1@al.insper.edu.br>
2022-08-30 21:58:17 -0300

    resolvendo issue 1

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -4,12 +4,53 @@
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
+/*---------------- DEFINES ----------------*/
+#define BUZZER_PIO		PIOC
+#define BUZZER_PIO_ID	ID_PIOC
+#define BUZZER_PIO_IDX	13
+#define BUZZER_PIO_IDX_MASK	(1 << BUZZER_PIO_IDX)
+
+/*---------------- PROTÓTIPOS ----------------*/
+
+// Pôem 1 no PINO do buzzer
+void set_buzzer();
+
+// Põem 0 no PINO do buzzer
+void clear_buzzer();
+
+/*---------------- FUNCOES ----------------*/
+
+void io_init(){
+	
+	// Desativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	// Inicializa PIO do buzzer
+	pmc_enable_periph_clk(BUZZER_PIO_ID);
+	
+	// Setando PINO do buzzer como output
+	pio_set_output(BUZZER_PIO, BUZZER_PIO_IDX_MASK, 1, 0, 0);
+}
+
+
+void set_buzzer(){
+	pio_set(BUZZER_PIO, BUZZER_PIO_IDX_MASK);
+}
+
+void clear_buzzer(){
+	pio_clear(BUZZER_PIO, BUZZER_PIO_IDX_MASK);  
+}
+
+/*----------------- MAIN -----------------*/
 int main (void)
 {
 	board_init();
 	sysclk_init();
 	delay_init();
-
+	
+  // Init button
+	io_init();
+	
   // Init OLED
 	gfx_mono_ssd1306_init();
   
