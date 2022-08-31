4068836bf4fb6aa0d6111e59cc40c6dc14a8c005
leticiacb1 <leticiacb1@al.insper.edu.br>
2022-08-30 23:21:29 -0300

    resolvendo issue 3 , botao selecao

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -18,6 +18,13 @@
 #define START_PIO_IDX	28
 #define START_PIO_IDX_MASK	(1 << BUT1_IDX)
 
+// Botão SELECT
+#define SELECAO_PIO		PIOC
+#define SELECAO_PIO_ID	ID_PIOC
+#define SELECAO_PIO_IDX	31
+#define SELECAO_PIO_IDX_MASK (1 << SELECAO_PIO_IDX)
+
+
 /*---------------- PROTÓTIPOS ----------------*/
 
 // Pôem 1 no PINO do buzzer
@@ -26,24 +33,32 @@ void set_buzzer();
 // Põem 0 no PINO do buzzer
 void clear_buzzer();
 
-// Retorna status do botão (1/0)
+// Retorna status do botão START (1/0)
 int get_startstop();
+
+// Retorna status do botão SELECAO (1/0)
+int get_selecao();
 
 /*---------------- FUNCOES ----------------*/
 
 void io_init(){
 	
-	// Desativa WatchDog Timer
+	// -- Desativa WatchDog Timer --
 	WDT->WDT_MR = WDT_MR_WDDIS;
 	
-	// Inicializa PIO
+	// -- Inicializa PIO --
 	pmc_enable_periph_clk(BUZZER_PIO_ID);
 	pmc_enable_periph_clk(START_PIO_ID);
+	pmc_enable_periph_clk(SELECAO_PIO_ID);
 	
-	// Configurando PINOS
+	// -- Configurando PINOS --
 	pio_set_output(BUZZER_PIO, BUZZER_PIO_IDX_MASK, 1, 0, 0);
+	
+	// Inputs
 	pio_configure(START_PIO, PIO_INPUT, START_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
 	pio_set_debounce_filter(START_PIO, START_PIO_IDX_MASK, 60);
+	pio_configure(SELECAO_PIO, PIO_INPUT, SELECAO_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(SELECAO_PIO, SELECAO_PIO_IDX_MASK, 60);
 }
 
 
@@ -57,6 +72,10 @@ void clear_buzzer(){
 
 void get_startstop(){
 	return pio_get(START_PIO,PIO_INPUT,START_PIO_IDX_MASK);
+}
+
+int get_selecao(){
+	return pio_get(SELECAO_PIO,PIO_INPUT,SELECAO_PIO_IDX_MASK);
 }
 
 /*----------------- MAIN -----------------*/
